452c80bcc11ed6c0fcbe918c7f5eaf41eee5ab9f
Luciano Santos <luchosan74@gmail.com>
2017-09-29 18:47:27 -0300

    Implementado ABM y consulta de marcas en la base de datos #4

--- model/MarcasModel.php
+++ model/MarcasModel.php
@@ -9,7 +9,27 @@
       $sentencia->execute();
       return $sentencia->fetchAll(PDO::FETCH_ASSOC);
     }
+    function getMarca($id_marca){
+      $sentencia = $this->db->prepare( "select * from marca where id_marca = ? limit 1");
+      $sentencia->execute([$id_marca]);
+      return $sentencia->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function borrarMarca($id_marca){
+      $sentencia = $this->db->prepare( "delete from marca where id_marca=?");
+      $sentencia->execute([$id_tarea]);
+    }
+    function cargarMarca($nombre, $descripcion){
+      $sentencia = $this->db->prepare('INSERT INTO tarea(nombre,descripcion) VALUES(?,?)');
+      $sentencia->execute([$nombre,$descripcion]);
+    }
+    function setNombreMarca($id_marca,$nombre){
+      $sentencia = $this->db->prepare( "update marca set nombre=? where id_marca=?");
+      $sentencia->execute([$nombre,$id_marca]);
+    }
+    function setDescripcionMarca($id_marca,$descripcion){
+      $sentencia = $this->db->prepare( "update marca set descripcion=? where id_marca=?");
+      $sentencia->execute([$descripcion,$id_marca]);
+    }
   }
-
 
  ?>
