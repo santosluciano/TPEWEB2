a8f56db5810d219ccf028e57057445b78744d0de
Luciano Santos <luchosan74@gmail.com>
2017-10-10 01:41:24 -0300

    se agrego una base de datos optimizada para importar #5

--- database/schema.sql
+++ database/schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 10-10-2017 a las 01:51:18
+-- Tiempo de generación: 10-10-2017 a las 06:39:40
 -- Versión del servidor: 10.1.26-MariaDB
 -- Versión de PHP: 7.1.8
 
