d99c33e6bbf7de2ef76a3bad6bdadff616d613ef
Luciano Santos <luchosan74@gmail.com>
2017-10-10 01:01:35 -0300

    Auto install de la base de datos funcionando #5

--- model/Model.php
+++ model/Model.php
@@ -6,9 +6,42 @@
 
     function __construct()
     {
-      $this->db = new PDO('mysql:host=localhost;'
-      .'dbname=db_celulares;charset=utf8'
-      , 'root', '');
+      try {
+        $this->db = new PDO('mysql:host=localhost;'
+        .'dbname=db_celulares;charset=utf8'
+        , 'root', '');
+      } catch (PDOException $e) {
+        $this->buildDDBBfromFile('db_celulares','schema.sql');
+      }
+    }
+    public function buildDDBBfromFile($dbname,$dbfile)
+    {
+      try {
+        $this->db = new PDO('mysql:host=localhost', 'root', '');
+        $this->db->exec('CREATE DATABASE IF NOT EXISTS '.$dbname); //Creacion SQL
+        $this->db->exec('USE '.$dbname); //Sentencia para usar por defecto la BBDD
+        $queries = $this->loadSQLSchema($dbfile); //Cargar el archivo de texto
+        $i = 0; // Ejecuto todos los queries
+        while ($i < count($queries) && strlen($this->db->errorInfo()[2]) == 0)
+        {
+          $this->db->exec($queries[$i]);
+          echo $queries[$i];
+          $i++;
+        }
+      } catch (Exception $e) {
+        echo $e;
+      }
+    }
+    public function loadSQLSchema($dbfile){
+      $querys = fopen("database/$dbfile", "r+");
+      $sql = "";
+      while (!feof($querys)) {
+        $linea = fgets($querys);
+        $sql .= $linea;
+      }
+      fclose($querys);
+      $querys = explode(";", $sql);
+      return $querys;
     }
   }
  ?>
