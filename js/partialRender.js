a93d2be744894b4a807f79ade5ff0bfa52fa7c50
Luciano Santos <luchosan74@gmail.com>
2017-10-11 15:26:58 -0300

    Modificada la base del sitio, falta hacerlo dinamico

--- js/partialRender.js
+++ js/partialRender.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
+//  llamada_ajax("home");
   $(document).on('click','.partial',function(event){
     event.preventDefault();
     let accion = this.href;
-    $.ajax({
-      url:accion,
-      success: function(result) {
-        $(".cuerpo").html(result);
-      }
-    });
+    llamada_ajax(accion);
     $(".cuerpo").html("Cargando...");
   });
   $('.partialSearch').on('submit',function(event){
@@ -19,4 +15,12 @@ $(document).ready(function () {
 			      		$(".cuerpo").html(response);
     });
   });
+  function llamada_ajax(accion){
+    $.ajax({
+      url:accion,
+      success: function(result) {
+        $(".cuerpo").html(result);
+      }
+    });
+  }
 });
